cd526bcaa4afe74757fe48631348ccc3d389cda9
Darren Phua <darrenwcphua41@gmail.com>
2020-04-30 18:33:36 -0500

    first commit

---
+++ Passwords.R
@@ -0,0 +1,5 @@
+# read the data from tidytuesday
+# passwords <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-14/passwords.csv')
+
+# read data on games taken from data.world
+games <- read.csv("https://query.data.world/s/4pach4x76ozrfmeavwsg6b4ja4qakj", header=TRUE, stringsAsFactors=FALSE);
